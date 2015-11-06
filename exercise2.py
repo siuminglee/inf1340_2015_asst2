e63f1faa240f8e37ab30320c214bcc2904ddf70d
siuminglee <siuming.lee@mail.utoronto.ca>
2015-11-05 19:28:04 -0500

    complete

--- exercise2.py
+++ exercise2.py
@@ -16,32 +16,45 @@ def find(input_string, substring, start, end):
     """
     Describe your function
 
-    :param :
-    :return:
+    :param input_string : list to be searched
+    :param substring : string you are searching for
+    :return: lowest index where substring is found in input_string
+                if substring is not found in input_string, returns -1
     :raises:
 
     """
-    index = 0
-    r = 0
 
-    while index < len(input_string):
-        if input_string[index] == substring[r]:
-            while len(substring)
+    substring = str(substring)
+    input_string = str(input_string)
 
+    if substring in input_string:
+        for index in range(len(input_string)):
+            if input_string[index:index+len(substring)] == substring[0:]:
+                return index
+    else:
+        return -1
 
-    return -1
-
+print find("Hello, are you my mummy?", "my", 0, 23)
 
 def multi_find(input_string, substring, start, end):
     """
     Describe your function
 
-    :param :
-    :return:
+    :param input_string : list to be searched
+    :param substring : string you are searching for
+    :return: a string with zero or more indices separated by commas
     :raises:
 
     """
-    result = ""
+    substring = str(substring)
+    input_string = str(input_string)
 
-    return result
+
+
+    for index in range(len(input_string)):
+        if input_string[index:index+len(substring)] == substring:
+            print index, ",",
+            index += 1
+
+multi_find("Hello, are you my mummy?", "my", 0, 23)
 
--- test_exercise2.py
+++ test_exercise2.py
@@ -19,10 +19,16 @@ def test_find_basic():
     Test find function.
     """
     assert find("This is an ex-parrot", "parrot", 0, 20) == 14
+    assert find("Hello, are you my mummy?", "my", 0, 23) == 15
+    assert find("You are my sunshine", "moonlight", 0, 18) == -1
+    assert find("abcdefghijklmnop", "abd", 0, 16) == -1
 
 
 def test_multi_find_basic():
     """
     Test multi_find function.
     """
-    assert multi_find("Ni! Ni! Ni! Ni!", "Ni", 0, 20) == "0,4,8,12"
+    assert multi_find("Ni! Ni! Ni! Ni!", "Ni", 0, 20) == "0 , 4 , 8 , 12 ,"
+    assert multi_find("Hello, are you my mummy?", "my", 0, 23) == "15 , 21 ,"
+    assert multi_find("This is a sentence", "notinsentence", 0, 18) == " , "
+    assert multi_find("Wibbley wobbly timey wimey stuff", "ey", 0, 32) == "6 , 19 , 25 ,"
